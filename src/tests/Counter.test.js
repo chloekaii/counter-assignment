b611bccaa2d9310f968d4d1dafb26025fc178154
chloekaii <116681534+chloekaii@users.noreply.github.com>
2024-11-19 15:11:43 -0500

    added unit tests

---
+++ src/tests/Counter.test.js
@@ -0,0 +1,35 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import Counter from '../components/Counter';
+
+beforeEach(() => {
+  // Render the Counter component here
+  render(<Counter />);
+})
+
+test('renders counter message', () => {
+  // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText(/Counter/i)).toBeInTheDocument();
+  expect(screen.getByTestId("count")).toBeInTheDocument();
+});
+
+test('should render initial count with value of 0', () => {
+  // Complete the unit test below based on the objective in the line above
+  const countElement = screen.getByTestId("count");
+  expect(countElement).toHaveTextContent("0");
+});
+
+test('clicking + increments the count', () => {
+  // Complete the unit test below based on the objective in the line above
+  const incrementButton = screen.getByText("+");
+  fireEvent.click(incrementButton);
+  const countElement = screen.getByTestId("count");
+  expect(countElement).toHaveTextContent("1");
+});
+
+test('clicking - decrements the count', () => {
+  // Complete the unit test below based on the objective in the line above
+  const decrementButton = screen.getByText("-");
+  fireEvent.click(decrementButton);
+  const countElement = screen.getByTestId("count");
+  expect(countElement).toHaveTextContent("-1");
+});
